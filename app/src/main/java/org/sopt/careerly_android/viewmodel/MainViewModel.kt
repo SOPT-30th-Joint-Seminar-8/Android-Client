cea6b5ae1e73f1dd665b4c83431b203e1cbcafa6
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-05-17 06:01:57 +0900

    [ADD] #2 - 메인뷰모델 초기화

---
+++ app/src/main/java/org/sopt/careerly_android/viewmodel/MainViewModel.kt
@@ -0,0 +1,33 @@
+package org.sopt.careerly_android.viewmodel
+
+import android.graphics.Bitmap
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import org.sopt.careerly_android.util.ImageDownloadManager.getImage
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : ViewModel() {
+    private val _profileBitmap = MutableLiveData<Bitmap?>()
+    val profileBitmap: LiveData<Bitmap?>
+        get() = _profileBitmap
+
+    @DelicateCoroutinesApi
+    @ExperimentalCoroutinesApi
+    fun getProfileBitmap() {
+        viewModelScope.launch {
+            _profileBitmap.value = getImage(EXAMPLE_PROFILE_IMAGE)
+        }
+    }
+
+    companion object {
+        const val EXAMPLE_PROFILE_IMAGE =
+            "http://imagescdn.gettyimagesbank.com/500/201905/jv11366256.jpg"
+    }
+}
