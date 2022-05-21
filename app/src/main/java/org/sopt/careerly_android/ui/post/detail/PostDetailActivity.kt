876453f189a19f7e4210104b6d49f023e62f9587
seeeuni <seeun0905@cau.ac.kr>
2022-05-22 00:25:36 +0900

    [UI] #7 - 댓글 리사이클러뷰 완성

--- app/src/main/java/org/sopt/careerly_android/ui/post/detail/PostDetailActivity.kt
+++ app/src/main/java/org/sopt/careerly_android/ui/post/detail/PostDetailActivity.kt
@@ -5,11 +5,60 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import org.sopt.careerly_android.R
+import org.sopt.careerly_android.databinding.ActivityPostDetailBinding
+import org.sopt.careerly_android.ui.ReviewAdapter
+import org.sopt.careerly_android.ui.ReviewData
+import org.sopt.careerly_android.util.binding.BindingActivity
+import org.sopt.careerly_android.viewmodel.MainViewModel.Companion.EXAMPLE_PROFILE_IMAGE
 
-class PostDetailActivity : AppCompatActivity() {
+class PostDetailActivity : BindingActivity<ActivityPostDetailBinding>(R.layout.activity_post_detail) {
+    private lateinit var reviewAdapter: ReviewAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_post_detail)
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        reviewAdapter = ReviewAdapter()
+        binding.rvReview.adapter = reviewAdapter
+        reviewAdapter.reviewList.addAll(
+            listOf(
+                ReviewData(
+                    "박세은",
+                    "2시간 전",
+                    "Product Designer.@hyp...",
+                    "좋은 정보 감사합니다.",
+                    EXAMPLE_PROFILE_IMAGE
+                ), ReviewData(
+                    "박세은",
+                    "2시간 전",
+                    "Product Designer.@hyp...",
+                    "좋은 정보 감사합니다.",
+                    EXAMPLE_PROFILE_IMAGE
+                ), ReviewData(
+                    "박세은",
+                    "2시간 전",
+                    "Product Designer.@hyp...",
+                    "좋은 정보 감사합니다.",
+                    EXAMPLE_PROFILE_IMAGE
+                ),
+                ReviewData(
+                    "박세은",
+                    "2시간 전",
+                    "Product Designer.@hyp...",
+                    "좋은 정보 감사합니다.",
+                    EXAMPLE_PROFILE_IMAGE
+                ),
+                ReviewData(
+                    "박세은",
+                    "2시간 전",
+                    "Product Designer.@hyp...",
+                    "좋은 정보 감사합니다.",
+                    EXAMPLE_PROFILE_IMAGE
+                )
+            )
+        )
+        reviewAdapter.notifyDataSetChanged()
     }
 
     companion object {
