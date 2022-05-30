fac447fad1847695844c5471b6ec357da959a9ca
lee989898 <90037701+lee989898@users.noreply.github.com>
2022-05-31 08:58:00 +0900

    [ADD] #12 - Response Get DTO 추가

---
+++ app/src/main/java/org/sopt/careerly_android/data/remote/dto/response/ResponseGetDTO.kt
@@ -0,0 +1,44 @@
+package org.sopt.careerly_android.data.remote.dto.response
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseGetDTO(
+    @SerializedName("data")
+    val data: Data
+)
+
+data class Data(
+    @SerializedName("hotProfiles")
+    val hotProfiles: List<HotProfile>,
+    @SerializedName("posts")
+    val posts: List<Post>
+)
+
+data class Post(
+    @SerializedName("createdAt")
+    val createdAt: String,
+    @SerializedName("likes")
+    val likes: Int,
+    @SerializedName("postId")
+    val postId: String,
+    @SerializedName("text")
+    val text: String,
+    @SerializedName("userEmail")
+    val userEmail: String,
+    @SerializedName("userImg")
+    val userImg: String,
+    @SerializedName("userName")
+    val userName: String,
+    @SerializedName("views")
+    val views: Int
+)
+
+data class HotProfile(
+    @SerializedName("followers")
+    val followers: Int,
+    @SerializedName("job")
+    val job: String,
+    @SerializedName("userName")
+    val userName: String
+)
