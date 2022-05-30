376875838bb21789546f38a70723db31d471d4d2
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-05-30 19:49:02 +0900

    [ADD] #15 - 데이터 로직 관련 Hilt 모듈 추가

---
+++ app/src/main/java/org/sopt/careerly_android/di/ServiceModule.kt
@@ -0,0 +1,18 @@
+package org.sopt.careerly_android.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.sopt.careerly_android.data.remote.service.PostService
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServiceModule {
+    @Provides
+    @Singleton
+    fun providePostService(retrofit: Retrofit): PostService =
+        retrofit.create(PostService::class.java)
+}
