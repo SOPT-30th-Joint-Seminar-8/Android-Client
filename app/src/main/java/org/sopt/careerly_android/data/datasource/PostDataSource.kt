42544c37b12c67c857ec1964209436c015972e05
lee989898 <90037701+lee989898@users.noreply.github.com>
2022-05-31 09:02:29 +0900

    [ADD] #12 - PostDataSource, PostDataSourceImpl get post list 추가

--- app/src/main/java/org/sopt/careerly_android/data/datasource/PostDataSource.kt
+++ app/src/main/java/org/sopt/careerly_android/data/datasource/PostDataSource.kt
@@ -2,8 +2,11 @@ package org.sopt.careerly_android.data.datasource
 
 import org.sopt.careerly_android.data.remote.dto.BaseResponse
 import org.sopt.careerly_android.data.remote.dto.request.RequestPostDTO
+import org.sopt.careerly_android.data.remote.dto.response.ResponseGetDTO
 import org.sopt.careerly_android.data.remote.dto.response.ResponsePostDTO
 
 interface PostDataSource {
     suspend fun postPostWrite(data: RequestPostDTO): BaseResponse<ResponsePostDTO>
+
+    suspend fun getPostList(): BaseResponse<ResponseGetDTO>
 }
