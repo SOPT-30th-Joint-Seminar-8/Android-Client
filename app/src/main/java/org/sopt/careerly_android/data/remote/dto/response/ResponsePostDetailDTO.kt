8a45a32e9c49021d2a02484d610e5e088ca2ec30
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-06-03 05:18:03 +0900

    [FEAT] #20 - 리스폰스 데이터 클래스 생성

---
+++ app/src/main/java/org/sopt/careerly_android/data/remote/dto/response/ResponsePostDetailDTO.kt
@@ -0,0 +1,10 @@
+package org.sopt.careerly_android.data.remote.dto.response
+
+data class ResponsePostDetailDTO(
+    val reviewId: String,
+    val userName: String,
+    val userEmail: String,
+    val userImg: String,
+    val text: String,
+    val createdAt: String
+)
